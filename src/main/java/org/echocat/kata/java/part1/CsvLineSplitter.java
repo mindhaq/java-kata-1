99a0358c933d53596ebd0b0ccf73201996e8e8ca
Rüdiger Schulz <rueschu@gmail.com>
2018-03-29 15:02:02 +0200

    split by semicolon

--- src/main/java/org/echocat/kata/java/part1/CsvLineSplitter.java
+++ src/main/java/org/echocat/kata/java/part1/CsvLineSplitter.java
@@ -1,10 +1,19 @@
 package org.echocat.kata.java.part1;
 
+import java.util.regex.Pattern;
+
 /**
  * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
  */
 public class CsvLineSplitter {
-    public CsvLineSplitter(String splitPattern) {
 
+    private final Pattern pattern;
+
+    public CsvLineSplitter(String splitPattern) {
+        pattern = Pattern.compile(splitPattern);
+    }
+
+    public String[] split(String line) {
+        return pattern.split(line);
     }
 }
--- src/test/java/org/echocat/kata/java/part1/CsvLineSplitterTest.java
+++ src/test/java/org/echocat/kata/java/part1/CsvLineSplitterTest.java
@@ -2,7 +2,7 @@ package org.echocat.kata.java.part1;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
@@ -12,7 +12,19 @@ public class CsvLineSplitterTest {
     private final CsvLineSplitter splitter = new CsvLineSplitter(";");
 
     @Test
-    public void foo() {
+    public void splits_thres_columns_into_three_strings() {
+        // given
+        String line = "null-walter@echocat.org;Paul;Walter";
 
+        // when
+        String[] strings = splitter.split(line);
+
+        // then
+        assertThat(strings).hasSize(3);
+        assertThat(strings).containsExactly(
+                "null-walter@echocat.org",
+                "Paul",
+                "Walter"
+        );
     }
 }
