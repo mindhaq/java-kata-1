d12512cbb11eafae1bdf7a46dbc4e937df72ec6e
Rüdiger Schulz <rueschu@gmail.com>
2018-03-29 15:21:04 +0200

    add AuthorConstructor

---
+++ src/main/java/org/echocat/kata/java/part1/reader/AuthorConstructor.java
@@ -0,0 +1,7 @@
+package org.echocat.kata.java.part1.reader;
+
+/**
+ * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
+ */
+public class AuthorConstructor {
+}
---
+++ src/test/java/org/echocat/kata/java/part1/reader/AuthorConstructorTest.java
@@ -0,0 +1,12 @@
+package org.echocat.kata.java.part1.reader;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
+ */
+public class AuthorConstructorTest {
+
+    private final AuthorConstructor authorConstructor = new AuthorConstructor();
+
+}
