c390cc2159dc97016e43da9490201a15a1ee19e8
Rüdiger Schulz <rueschu@gmail.com>
2018-03-29 14:52:13 +0200

    add CsvLineSplitter class

---
+++ src/main/java/org/echocat/kata/java/part1/CsvLineSplitter.java
@@ -0,0 +1,10 @@
+package org.echocat.kata.java.part1;
+
+/**
+ * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
+ */
+public class CsvLineSplitter {
+    public CsvLineSplitter(String splitPattern) {
+
+    }
+}
---
+++ src/test/java/org/echocat/kata/java/part1/CsvLineSplitterTest.java
@@ -0,0 +1,18 @@
+package org.echocat.kata.java.part1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Rüdiger Schulz &lt;rs@mindhaq.com&gt;
+ */
+public class CsvLineSplitterTest {
+
+    private final CsvLineSplitter splitter = new CsvLineSplitter(";");
+
+    @Test
+    public void foo() {
+
+    }
+}
